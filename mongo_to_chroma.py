d98740688f188cd9257af079ff5a10f605b038e4
LTuccillo95 <tuccillo.luigi95@gmail.com>
2024-07-05 09:59:28 +0200

    Add files via upload

--- mongo_to_chroma.py
+++ mongo_to_chroma.py
@@ -1,18 +1,30 @@
 from langchain_core.documents import Document
 from langchain.document_loaders import MongodbLoader
 from langchain_community.vectorstores.chroma import Chroma
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 loader = MongodbLoader(
     connection_string="mongodb://localhost:27017/",
     db_name='admin',
-    collection_name='src-attribute',
+    collection_name='src-spell',
 )
 
+# loader = MongodbLoader(
+#     connection_string="mongodb://localhost:27017/",
+#     db_name='admin',
+#     collection_name='src-attribute',
+# )
+
 docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=128, chunk_overlap=32)
+splitted_docs = text_splitter.split_documents(docs)
 
 embeddings = SentenceTransformerEmbeddings(
     model_name="all-MiniLM-L6-v2"
 )
 
-vectorDB = Chroma.from_documents(docs, embeddings, persist_directory='./chromaDB', collection_name="src_5e_attribute")
+# vectorDB = Chroma.from_documents(docs, embeddings, persist_directory='./chromaDB', collection_name="src_5e_splitted_spell")
+vectorDB = Chroma.from_documents(splitted_docs, embeddings, persist_directory='./chromaDB', collection_name="src_5e_splitted_spell")
 vectorDB.persist()
